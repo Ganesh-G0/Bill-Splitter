07ffea2426c37aea70c2b01c6d3208b84c9ae71e
Ganesh <121486938+Ganesh-G0@users.noreply.github.com>
2024-03-07 14:00:38 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,32 +2,36 @@ let tip = 0;
 document.querySelectorAll('.btn').forEach(function(button) {
   button.addEventListener('click', function() {
     tip = ($(this).val()).slice(0, -1);
-    console.log(tip);
+    console.log("Tip form btn: " + tip);
   });
   button.addEventListener('input', function() {
     tip = $(this).val();
-    console.log($(this).val());
+    console.log("Tip form input: " + tip);
   });
 });
-document.addEventListener('input', () => {
-  let bill = $("#bill").val();
-  let numberOfPeople = $("#numberOfPeople").val();
-  console.log(bill);
-  console.log(tip);
-  console.log(numberOfPeople);
-  // bill = 142.55; tip = 15; numberOfPeople = 5;
-  if (numberOfPeople != 0 && bill != 0 && tip != 0) {
-    let totalTip = bill * (tip / 100);
-    let totalBill = Number(totalTip) + Number(bill);
-    let totalTipPerPerson = (totalTip / numberOfPeople - 0.01).toFixed(2);
-    let totalBillPerPerson = (totalBill / numberOfPeople).toFixed(2);    
-    $("#totalTipAmtPerPerson").text(totalTipPerPerson);
-    $("#totalBillAmtPerPerson").text(totalBillPerPerson);
-    $("#zero").addClass("d-none");
-  } else { 
-    $("#zero").removeClass("d-none");
-    console.log("Else Not a Number");
-  }  
+document.querySelector("#numberOfPeople").addEventListener("input", function() {
+  document.addEventListener('input', () => {
+    let bill = $("#bill").val();
+    let numberOfPeople = $("#numberOfPeople").val();
+    if (numberOfPeople != 0 && bill != 0 && tip != 0) {
+      let totalTip = bill * (tip / 100);
+      let totalBill = Number(totalTip) + Number(bill);
+      let totalTipPerPerson = (totalTip / numberOfPeople).toFixed(2);
+      let totalBillPerPerson = (totalBill / numberOfPeople).toFixed(2);    
+      // $("#totalTipAmtPerPerson").text(with2Decimals(totalTipPerPerson));
+      $("#totalTipAmtPerPerson").text(totalTipPerPerson);
+      $("#totalBillAmtPerPerson").text(totalBillPerPerson);
+      $("#zero").addClass("d-none");
+      $("#numberOfPeople").removeClass("b-2");
+      console.log("Bill: " + bill);
+      console.log("Tip: " + tip);
+      console.log("People: " + numberOfPeople);
+    } else { 
+      $("#zero").removeClass("d-none");
+      $("#numberOfPeople").addClass("b-2");
+      console.log("Number is a Zero");
+    }  
+  })
 })
 $('#reset').click(()=> {
   $("#bill").val("");
@@ -35,3 +39,9 @@ $('#reset').click(()=> {
   $("#totalTipAmtPerPerson").text('0.00');
   $("#totalBillAmtPerPerson").text('0.00');
 })
+// function with2Decimals(num) {
+//   let ogNum = num;
+//   let newNum = num.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]; 
+//   console.log(num, newNum);
+//   return newNum;
+// }
